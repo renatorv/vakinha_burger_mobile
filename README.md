3f18cde460682ac2b50426126baab87afbdc6fe5
Renato Alves Pereira <renatorv@gmail.com>
2022-02-01 18:27:52 -0300

    parei: 28 minutos da aula 2

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+* **************************************** *
+* Extensões:                               *
+* To Export Extensions                     *
+* Run command palette Ctrl + Shift + P     *
+* Type VSC Export                          *
+*                                          *
+* To Import Extensions                     *
+* Run command palette Ctrl + Shift + P     *
+* Type VSC Import                          *
+* **************************************** *
---
+++ lib/app/repositories/auth/auth_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'dart:developer';
+
+import 'package:vakinha_burger_mobile/app/core/rest_client/rest_client.dart';
+import 'package:vakinha_burger_mobile/app/models/user_model.dart';
+
+import './auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final RestClient _restClient;
+
+  AuthRepositoryImpl({
+    required RestClient restClient,
+  }) : _restClient = restClient;
+
+  @override
+  Future<UserModel> register(String name, String email, String password) async {
+    final result = await _restClient.post(
+      '/auth/register',
+      {
+        'name': name,
+        'email': email,
+        'password': password,
+      },
+    );
+
+    if (result.hasError) {
+      var message = 'Erro ao registrar usuário';
+      // 400: email existe ou algo nesse tipo
+      if (result.statusCode == 400) {
+        message = result.body['error'];
+      }
+
+      log(
+        message,
+        error: result.statusText,
+        stackTrace: StackTrace.current,
+      );
+
+      throw RestClientException(message);
+    }
+
+    return UserModel.fromJson('text');
+  }
+}
+
+parei: 28 minutos da aula 2
