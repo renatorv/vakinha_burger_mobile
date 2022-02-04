88e8261bd8d2110a6d1fdd14702f38508e3ea91f
Renato Alves Pereira <renatorv@gmail.com>
2022-02-04 08:37:35 -0300

    Dia 3 - 40 minutos

---
+++ lib/app/core/services/auth_service.dart
@@ -0,0 +1,19 @@
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:vakinha_burger_mobile/app/core/constants/constants.dart';
+
+/// O GetX entende que um service, NUNCA deve morrer
+class AuthService extends GetxService {
+
+  // 
+  final isLogged = RxnBool();
+  // Instância do GetStorage
+  final _getStorage = GetStorage();
+
+  void logout() => _getStorage.write(Constants.USER_KEY, null);
+  
+}
+
+DW - 2022 - Dia 3
+40 minutos
