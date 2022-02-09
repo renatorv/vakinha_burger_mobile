4914901e3046d6e61c4a7f11b0efd8ff52888dff
Renato Alves Pereira <renatorv@gmail.com>
2022-02-09 07:58:20 -0300

    58 minutos do dia 3

--- lib/app/core/services/auth_service.dart
+++ lib/app/core/services/auth_service.dart
@@ -1,19 +1,46 @@
-
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:vakinha_burger_mobile/app/core/constants/constants.dart';
 
 /// O GetX entende que um service, NUNCA deve morrer
 class AuthService extends GetxService {
-
-  // 
-  final isLogged = RxnBool();
+  //
+  final _isLogged = RxnBool();
   // Instância do GetStorage
   final _getStorage = GetStorage();
 
-  void logout() => _getStorage.write(Constants.USER_KEY, null);
-  
-}
+  Future<AuthService> init() async {
+    _getStorage.listenKey(
+      Constants.USER_KEY,
+      (value) {
+        // if (value != null) {
+        //   isLogged(true);
+        // } else {
+        //   isLogged(false);
+        // }
+        _isLogged(value != null);
+      },
+    );
 
-// DW - 2022 - Dia 3
-// 40 minutos
+    ever<bool?>(
+      _isLogged,
+      (isLogged) {
+        if (isLogged == null || isLogged) {
+          Get.offAllNamed('/auth/login');
+        }else{
+          Get.offAllNamed('/home');
+        }
+      },
+    );
+
+    // final isLoggedData = getUserId() != null;
+    _isLogged(getUserId() != null);
+
+    return this;
+  }
+
+  // Usuário não logado mais
+  void logout() => _getStorage.write(Constants.USER_KEY, null);
+
+  int? getUserId() => _getStorage.read(Constants.USER_KEY);
+}
